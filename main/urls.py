a80651b254e8bdf1c76479c820e9c9d89ed8e136
ahsanshaokat <ahsan.shaokat@gmail.com>
2021-11-21 22:41:39 +0500

    d

--- main/urls.py
+++ main/urls.py
@@ -48,21 +48,21 @@ urlpatterns = [
    
     url(r'^profile/$',views.user_profile, name="profile"),
     
-    url(r'^reset-password/$', password_reset, {'template_name':'main/reset_password.html',
+    url(r'^reset-password/$', PasswordResetView.as_view(), {'template_name':'main/reset_password.html',
                                                     'email_template_name':'main/reset_password_email.html',
                                                     'post_reset_redirect':'main:password_reset_done',
                                                     'from_email':'main@django.com',},name='password_reset'),
 
    
-    url(r'^reset-password/done/$', password_reset_done, {'template_name': 'main/reset_password_done.html'},
+    url(r'^reset-password/done/$', PasswordResetDoneView.as_view(), {'template_name': 'main/reset_password_done.html'},
                                                          name='password_reset_done'),
 
     url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-    password_reset_confirm, {'template_name':'main/reset_password_confirm.html',
+    PasswordResetConfirmView.as_view(), {'template_name':'main/reset_password_confirm.html',
                               'post_reset_redirect': reverse_lazy('main:password_reset_complete')},
                               name='password_reset_confirm'),
 
-    url(r'reset-password/complete/$', password_reset_complete,{'template_name':'main/reset_password_complete.html'}, name='password_reset_complete'),
+    url(r'reset-password/complete/$', PasswordResetCompleteView.as_view(),{'template_name':'main/reset_password_complete.html'}, name='password_reset_complete'),
 
     url(r'^favorite/$',UserFavoriteProperty.as_view(),name='favorite'),
 
